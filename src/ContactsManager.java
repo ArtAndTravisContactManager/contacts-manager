f9ae1c54d8145da09cc2b993c97e13444b6b6c1b
Travis Payne <thpaggie910@gmail.com>
2017-10-04 15:55:11 -0500

    Add display menu method and start handling user input in ContactsManager application

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,4 +1,5 @@
 import util.FileHandler;
+import util.Input;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,19 +13,29 @@ public class ContactsManager {
         ArrayList<Contact> contacts = new ArrayList<>();
         FileHandler contactsFile = new FileHandler("./resources", "contacts.txt");
         List<String> contactsFileContents = contactsFile.readFromFile();
+        Input console = new Input();
+        int userChoice;
+
         contactsFile.getFilePath();
 
         for (String line : contactsFileContents) {
             String[] parts = line.split(",");
             String name = parts[0];
             String phoneNumber = parts[1];
-//            String name = line.split(",")[0];
-//            String phoneNumber = line.split(",")[1];
             Contact currentContact = new Contact(name, phoneNumber);
             contacts.add(currentContact);
+        }
+
+        displayMenu();
+        userChoice = console.getInt(1, 5, "Enter an option (1, 2, 3, 4, or 5): ");
+        System.out.println();
+
+        switch (userChoice) {
+            case 1:
+                displayContacts(contacts);
+                break;
 
         }
-        displayContacts(contacts);
     }
 
     public static void displayContacts(ArrayList<Contact> contacts) {
@@ -33,6 +44,15 @@ public class ContactsManager {
         for (Contact contact: contacts){
             System.out.println(contact.getName() + " | " + contact.getPhoneNumber());
         }
+    }
+
+    public static void displayMenu() {
+        System.out.println("Welcome to your Contacts Manager!\n");
+        System.out.println("1. View contacts.");
+        System.out.println("2. Add a new contact.");
+        System.out.println("3. Search a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit.");
     }
 }
 
