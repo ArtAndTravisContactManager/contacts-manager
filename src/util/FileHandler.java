4f4d307fb513fc89c5c5f9476b368e3bbf851838
Art <art_jr2000@yahoo.com>
2017-10-04 11:42:53 -0500

    Created FileHandlerTest and tested for errors -Created constructor in FileHandler

--- src/util/FileHandler.java
+++ src/util/FileHandler.java
@@ -1,5 +1,30 @@
 package util;
 
-public class FileHandler {
+//import java.nio.file;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class FileHandler {
+    private Path directory;
+    private Path filename;
+
+    public FileHandler(String directory, String filename) {
+        this.directory = Paths.get(directory);
+        this.filename = Paths.get(directory, filename);
+    }
+
+    public void createFile (){
+        try {
+            if (Files.notExists(filename)) {
+                Files.createFile(filename);
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
+
+
---
+++ src/util/FileHandlerTest.java
@@ -0,0 +1,11 @@
+package util;
+
+public class FileHandlerTest {
+
+    public static void main(String[] args) {
+        FileHandler fh = new FileHandler(".", "test.txt");
+        fh.createFile();
+
+
+    }
+}
