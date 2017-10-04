cba2710c62c471cad0dd63e1c6b542af984351e5
Art <art_jr2000@yahoo.com>
2017-10-04 15:41:26 -0500

    added display method to ContactsManager

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -10,14 +10,30 @@ public class ContactsManager {
         // Search contacts by name
         // Delete existing contact
         ArrayList<Contact> contacts = new ArrayList<>();
-        FileHandler contactsFile = new FileHandler("../resources", "contacts.txt");
+        FileHandler contactsFile = new FileHandler("./resources", "contacts.txt");
         List<String> contactsFileContents = contactsFile.readFromFile();
+        contactsFile.getFilePath();
 
-        for(String line : contactsFileContents) {
-            String name = line.split(",")[0];
-            String phoneNumber = line.split(",")[1];
+        for (String line : contactsFileContents) {
+            String[] parts = line.split(",");
+            String name = parts[0];
+            String phoneNumber = parts[1];
+//            String name = line.split(",")[0];
+//            String phoneNumber = line.split(",")[1];
             Contact currentContact = new Contact(name, phoneNumber);
             contacts.add(currentContact);
+
+        }
+        displayContacts(contacts);
+    }
+
+    public static void displayContacts(ArrayList<Contact> contacts) {
+        System.out.println("Name | Phone Number ");
+        System.out.println("---------------------------------");
+        for (Contact contact: contacts){
+            System.out.println(contact.getName() + " | " + contact.getPhoneNumber());
         }
     }
 }
+
+
