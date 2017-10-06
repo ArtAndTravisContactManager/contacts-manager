9a107d56b3e181379ccd6d24f843efd217b2ba43
Art <art_jr2000@yahoo.com>
2017-10-06 10:03:39 -0500

    added First and last name to add new contacts. formatted phone number with user input

--- src/Contact.java
+++ src/Contact.java
@@ -1,9 +1,11 @@
 public class Contact {
     private String name;
+    private String lastName;
     private String phoneNumber;
 
-    public Contact(String name, String phoneNumber) {
+    public Contact(String name, String lastname, String phoneNumber) {
         this.name = name;
+        this.lastName = lastname;
         this.phoneNumber = phoneNumber;
     }
 
@@ -14,4 +16,7 @@ public class Contact {
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
+    public String getLastName(){ return this.lastName; }
+
 }
